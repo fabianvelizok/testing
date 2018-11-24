c9542f7a64e33ab2f24e5779514debd72041e2a3
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-11-24 18:53:43 -0300

    Test an API that returns an error

--- __test__/async.test.js
+++ __test__/async.test.js
@@ -1,6 +1,7 @@
 import { getData } from '../src/promise';
 
-const API = 'http://www.mocky.io/v2/5becd53d3300002255fbc51d';
+const MOCKY_API = 'http://www.mocky.io/v2';
+const HTTP_STATUS_API = 'http://httpstat.us';
 
 describe('Async/Await', () => {
   const expectedResult = [
@@ -9,7 +10,15 @@ describe('Async/Await', () => {
   ];
 
   test('Mock API should return 2 items', async () => {
-    const response = await getData(API);
+    const response = await getData(`${MOCKY_API}/5becd53d3300002255fbc51d`);
     expect(response.length).toBeGreaterThan(1);
+  });
+
+  test('Http status API should return a 500 error', async () => {
+    try {
+      await getData(`${HTTP_STATUS_API}/500`);
+    } catch(error) {
+      expect(error).toEqual(new Error('Request failed with status code 500'));
+    }
   });
 });
